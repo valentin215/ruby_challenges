c01cfc324bf7aa6cd35e0edaa64654848c5e7697
Valentin Birault <biraultvalentin@gmail.com>
2020-02-02 20:00:07 +0100

    adding model into file to review

--- linked-list/linked_list.rb
+++ linked-list/linked_list.rb
@@ -38,3 +38,13 @@ class Deque
     out.value
   end
 end
+
+class Node
+  attr_accessor :value, :prev, :n
+
+  def initialize(value, prev=nil, n=nil)
+    @value = value
+    @prev  = prev
+    @next  = n
+  end
+end
